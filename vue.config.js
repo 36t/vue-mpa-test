8a211b4c56ed946f35c96e18f578f16464556bf7
36t <saluta@gmail.com>
2021-03-05 22:31:33 +0900

    refactor: excuted formatter.

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,17 @@
-const glob = require('glob')
-const titles = require('./src/pages/title.js')
-const pages = {}
+const glob = require('glob');
+const titles = require('./src/pages/title.js');
+const pages = {};
 
 glob.sync('./src/pages/**/app.ts').forEach(path => {
-  const chunk = path.split('./src/pages/')[1].split('/app.ts')[0]
-  console.log(chunk)
+  const chunk = path.split('./src/pages/')[1].split('/app.ts')[0];
   pages[chunk] = {
     entry: path,
     template: 'public/index.html',
     filename: `${chunk}.html`,
     title: titles[chunk],
     chunks: ['chunk-vendors', 'chunk-common', chunk]
-  }
-})
+  };
+});
 
 module.exports = {
   pages,
