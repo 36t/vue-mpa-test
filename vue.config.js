129ec7adac32aa8940f1cd4991aa3277f650f1ff
猿田 昌志 <saruta_masashi@cocone.co.jp>
2021-03-08 13:45:15 +0900

    mod: changed single quote to double.

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
-const glob = require('glob');
-const titles = require('./src/pages/title.config.js');
+const glob = require("glob");
+const titles = require("./src/pages/title.config.js");
 const pages = {};
 
-glob.sync('./src/pages/**/app.ts').forEach(path => {
-  const chunk = path.split('./src/pages/')[1].split('/app.ts')[0];
+glob.sync("./src/pages/**/app.ts").forEach(path => {
+  const chunk = path.split("./src/pages/")[1].split("/app.ts")[0];
   pages[chunk] = {
     entry: path,
-    template: 'public/index.html',
+    template: "public/index.html",
     filename: `${chunk}.html`,
     title: titles[chunk],
-    chunks: ['chunk-vendors', 'chunk-common', chunk]
+    chunks: ["chunk-vendors", "chunk-common", chunk]
   };
 });
 
